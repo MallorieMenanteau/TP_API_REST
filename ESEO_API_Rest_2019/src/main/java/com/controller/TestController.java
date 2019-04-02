c207135e34b58ba60e8fee469059cf21a26bf6b3
mallorie <mallorie@mallorie-HP-Pavilion-Notebook>
2019-04-02 10:52:28 +0200

    ajout des méthodes GET, POST, PUT, DELETE

--- ESEO_API_Rest_2019/src/main/java/com/controller/TestController.java
+++ ESEO_API_Rest_2019/src/main/java/com/controller/TestController.java
@@ -1,5 +1,11 @@
 package com.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.dao.VilleFranceDAO;
+import com.dto.VilleFranceDTO;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,4 +22,59 @@ public class TestController {
 		 
 		return value;
 	}
+	
+	@RequestMapping(value="/get",method=RequestMethod.GET)
+	@ResponseBody
+	public String getbdd(@RequestParam(required=false,value="value") String value) {
+		System.out.println("Appel GET");
+		VilleFranceDAO dao = new VilleFranceDAO();
+		List<VilleFranceDTO> liste = dao.getBDD();
+		for (int i=0; i<liste.size();i++)
+			System.out.println(liste.get(i).getNom());
+		 
+		return value;
+	}
+	
+	@RequestMapping(value="/getByCP",method=RequestMethod.GET)
+	@ResponseBody
+	public String getbddByCodePostal(@RequestParam(required=false,value="codePostal") String value) {
+		System.out.println("Appel GET");
+		VilleFranceDAO dao = new VilleFranceDAO();
+		List<VilleFranceDTO> liste = dao.getBDDparCodePostal(value);
+		for (int i=0; i<liste.size();i++)
+			System.out.println(liste.get(i).getNom());
+		 
+		return value;
+	}
+	
+	@RequestMapping(value="/post1",method=RequestMethod.POST)
+	@ResponseBody
+	public String post(@RequestParam(required=false,value="codeC") String code_commune) {
+
+		System.out.println("Appel POST");
+		return "fonctionne";
+	}
+	
+	@RequestMapping(value="/put",method=RequestMethod.PUT)
+	@ResponseBody
+	public void post(@RequestParam(required=true,value="codeC") String code_commune,
+					@RequestParam(required=true,value="aChanger") int indiceParamAChanger,
+					@RequestParam(required=true,value="param") String param) {
+
+		System.out.println("Appel PUT");
+		
+		VilleFranceDAO dao = new VilleFranceDAO();
+		dao.put(param,indiceParamAChanger,code_commune);	
+	}
+	
+	@RequestMapping(value="/delete",method=RequestMethod.DELETE)
+	@ResponseBody
+	public void delete(@RequestParam(required=true,value="codeC") String code_commune) {
+
+		System.out.println("Appel DELETE");
+		
+		VilleFranceDAO dao = new VilleFranceDAO();
+		dao.delete(code_commune);	
+	}
+	
 }
--- ESEO_API_Rest_2019/src/main/java/com/dao/VilleFranceDAO.java
+++ ESEO_API_Rest_2019/src/main/java/com/dao/VilleFranceDAO.java
@@ -1,24 +1,21 @@
 package com.dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.dto.VilleFranceDTO;
 
 import com.config.ConnexionBDD;
 
 public class VilleFranceDAO {
 	
-	private ConnexionBDD connexionbdd= new ConnexionBDD();
-	
-	public static final String ATT_CODE_COMMUNE = "Code_commune_INSEE";
-	public static final String ATT_NOM_COMMUNE = "Nom_commune";
-	public static final String ATT_CODE_POSTAL = "Code_postal";
-	public static final String ATT_LIBELLE_ACHEMINEMENT = "Libelle_acheminement";
-	public static final String ATT_LIGNE_5 = "Ligne_5";
-	public static final String ATT_LATITUDE = "Latitude";
-	public static final String ATT_LONGITUDE = "Longitude";
-	
-	private static final String SQL_SELECT = "SELECT * FROM ville_france ";
-	
+	private ConnexionBDD connexionbdd= new ConnexionBDD();	
 	
 	public VilleFranceDAO() {
 	}
@@ -27,6 +24,126 @@ public class VilleFranceDAO {
 		return this.connexionbdd.connexion();
 	}
 	
+	public List<VilleFranceDTO> getBDD() {
+		List<VilleFranceDTO> liste = new ArrayList<>();
+		try {
+			Connection connect = this.creerConnexion();
+			Statement stm = connect.createStatement();
+
+			ResultSet rset = stm.executeQuery("SELECT * FROM ville_france ORDER BY Nom_commune");
+			while(rset.next()) {
+				VilleFranceDTO ville = new VilleFranceDTO();
+				ville.setCode_commune(rset.getString(1));
+				ville.setNom(rset.getString(2));
+				ville.setCode_postal(rset.getString(3));
+				ville.setLatitude(rset.getString(6));
+				ville.setLibelle_acheminement(rset.getString(4));
+				ville.setLigne_5(rset.getString(5));
+				ville.setLongitude(rset.getString(7));
+				liste.add(ville);
+			}
+			
 	
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return liste;
+	}
+	
+	public List<VilleFranceDTO> getBDDparCodePostal(String codeP) {
+		List<VilleFranceDTO> liste = new ArrayList<>();
+		try {
+			Connection connect = this.creerConnexion();
+			Statement stm = connect.createStatement();
+
+			ResultSet rset = stm.executeQuery("SELECT * FROM ville_france WHERE Code_postal='"+codeP+"' ORDER BY Nom_commune");
+			while(rset.next()) {
+				VilleFranceDTO ville = new VilleFranceDTO();
+				ville.setCode_commune(rset.getString(1));
+				ville.setNom(rset.getString(2));
+				ville.setCode_postal(rset.getString(3));
+				ville.setLatitude(rset.getString(6));
+				ville.setLibelle_acheminement(rset.getString(4));
+				ville.setLigne_5(rset.getString(5));
+				ville.setLongitude(rset.getString(7));
+				liste.add(ville);
+			}
+			
+	
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return liste;
+	}
+	
+	public void post(VilleFranceDTO ville) {
+		String sql = "INSERT INTO ville_france VALUES (?,?,?,?,?,?,?)";
+		try {
+			Connection connect = this.creerConnexion();
+			PreparedStatement preparedStatement = connect.prepareStatement(sql); 
+
+			preparedStatement.setString(1,ville.getCode_commune()); 
+			preparedStatement.setString(2,ville.getNom()); 
+			preparedStatement.setString(3,ville.getCode_postal()); 
+			preparedStatement.setString(4,ville.getLibelle_acheminement()); 
+			preparedStatement.setString(5,ville.getLigne_5()); 
+			preparedStatement.setString(6,ville.getLatitude()); 
+			preparedStatement.setString(7,ville.getLongitude()); 
+
+			preparedStatement.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void put(String param, int indiceParametreAChanger,String code_commune) {
+		
+		List<String> requetes = new ArrayList<>();
+		requetes.add("UPDATE ville_france SET `Nom_commune`=? WHERE Code_commune_INSEE = ? ");
+		requetes.add("UPDATE ville_france SET `Code_postal`=? WHERE Code_commune_INSEE = ? ");
+		requetes.add("UPDATE ville_france SET `Libelle_acheminement`=? WHERE Code_commune_INSEE = ? ");
+		requetes.add("UPDATE ville_france SET `Ligne_5`=? WHERE Code_commune_INSEE = ? ");
+		requetes.add("UPDATE ville_france SET `Latitude`=? WHERE Code_commune_INSEE = ? ");
+		requetes.add("UPDATE ville_france SET `Longitude`=? WHERE Code_commune_INSEE = ? ");
+		
+		try {
+			Connection connect = this.creerConnexion();
+			PreparedStatement preparedStatement = connect.prepareStatement(requetes.get(indiceParametreAChanger-2)); 
+
+			preparedStatement.setString(1,param); 
+			preparedStatement.setString(2,code_commune); 
+
+
+			preparedStatement.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		
+		
+	}
+	
+	public void delete(String code_commune) {
+		String sql = "DELETE FROM ville_france WHERE Code_commune_INSEE = ?";
+		try {
+			Connection connect = this.creerConnexion();
+			PreparedStatement preparedStatement = connect.prepareStatement(sql); 
+
+			preparedStatement.setString(1,code_commune); 
+
+			preparedStatement.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 
 }
